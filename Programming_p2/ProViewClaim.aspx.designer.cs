0cb07143464797ac423453635458abb3500bbf3b
Olwethu Cwele <ST10344453@rcconnect.edu.za>
2024-10-24 13:42:49 +0000

    I have added lecture track status class class where he/she will track claim

--- Programming_p2/ProViewClaim.aspx.designer.cs
+++ Programming_p2/ProViewClaim.aspx.designer.cs
@@ -15,6 +15,24 @@ namespace Part2_Prog
     {
 
         /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// lblStatusMessage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblStatusMessage;
+
+        /// <summary>
         /// lblNoClaims control.
         /// </summary>
         /// <remarks>
